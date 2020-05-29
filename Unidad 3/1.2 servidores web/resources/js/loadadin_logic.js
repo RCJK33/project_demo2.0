44b5d7e3f3382a49564a7d8dc27eb4f0230e8aa9
RCJK33 <66058913+RCJK33@users.noreply.github.com>
2020-05-28 19:01:19 -0700

    Add files via upload

---
+++ Unidad 3/1.2 servidores web/resources/js/loadadin_logic.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',() =>{
+    const content_loading_pane = document.querySelector('.content_loading_pane')
+    content_loading_pane.style.opacity = 0
+    content_loading_pane.style.visibility = 'hidden'
+});
